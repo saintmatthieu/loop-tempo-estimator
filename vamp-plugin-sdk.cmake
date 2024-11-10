e53a8f4aab97cf21e2c51c335331f95667f9fbc2
kryksyh <kryksyh@gmail.com>
2024-11-10 13:16:22 +0300

    Enable CI builds Fix build on linux Disable vamp simple host build

--- vamp-plugin-sdk.cmake
+++ vamp-plugin-sdk.cmake
@@ -6,6 +6,6 @@ FetchContent_Declare(
   GIT_TAG 703438d8e55dbfd1c2021d0bc12e36edd1e227e2
 )
 
-set(VAMPSDK_BUILD_SIMPLE_HOST ON CACHE BOOL "Build simple host")
+set(VAMPSDK_BUILD_SIMPLE_HOST OFF CACHE BOOL "Build simple host")
 
 FetchContent_MakeAvailable(vamp-plugin-sdk)
